75a908810ce87bc91765a46d01aa0f64f8276036
Laurent Mbuyu <laurentmbuyu@Laurents-MacBook-Air.local>
2022-12-04 14:32:17 +0200

    Add minor message

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const pkgJSON = require('./package.json');
 const welcome = require('cli-welcome');
-
+const log = console.log;
 
 welcome({
     title: pkgJSON.name,
@@ -14,4 +14,22 @@ welcome({
     clear: true
 })
 
-console.log('Laurent Mbuyu');
+log(`
+Laurent Mbuyu
+
+Laurent is a software developer willing to do whatever it takes to deliver results. 
+Currently at Osiris Trading, and trying to raise above excellence by creating great solutions for the world. 
+Previously, Laurent built software geared towards the efficiency of use of plant machinery in Johannesburg for the BMW Group 
+right after serving as a software developer for a startup named Retro Rabbit.
+
+Twitter: https://twitter.com/laurent_mb
+Github: https://github.com/geek4ctrl
+`);
+
+console.log(`
+    SUCCESS: Thanks for checking out my CLI.
+    INFO: I'm busy thinking about life.
+    WARNING: Please don't copy me. Be yourself
+    ERROR: I'm on vacation. Contact me next week.
+`)
+
