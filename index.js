9ebbac427a0be9c8712e5c9cb692d7c9bcf34a54
Laurent Mbuyu <laurentmbuyu@Laurents-MacBook-Air.local>
2022-12-06 21:20:52 +0200

    Add data file for all the strings

--- index.js
+++ index.js
@@ -1,29 +1,16 @@
 #!/usr/bin/env node
-const log = console.log;
+
+const alert = require('cli-alertss');
+const handleError = require('cli-handle-error');
 
 const init = require('./utils/init');
+const data = require('./utils/data');
 
 (() => {
-
     init();
 
-    log(`
-    Laurent Mbuyu
-    
-    Laurent is a software developer willing to do whatever it takes to deliver results. 
-    Currently at Osiris Trading, and trying to raise above excellence by creating great solutions for the world. 
-    Previously, Laurent built software geared towards the efficiency of use of plant machinery in Johannesburg for the BMW Group 
-    right after serving as a software developer for a startup named Retro Rabbit.
-    
-    Twitter: https://twitter.com/laurent_mb
-    Github: https://github.com/geek4ctrl
-    `);
-    
-    console.log(`
-        SUCCESS: Thanks for checking out my CLI.
-        INFO: I'm busy thinking about life.
-        WARNING: Please don't copy me. Be yourself
-        ERROR: I'm on vacation. Contact me next week.
-    `);
+    console.log(data.bio);
+    console.log(data.social);
 
+    alert({ type: 'info', msg: data.ad })
 })();
---
+++ utils/data.js
@@ -0,0 +1,22 @@
+
+const bio = `Laurent Mbuyu
+    
+Laurent is a software developer willing to do whatever it takes to deliver results. 
+Currently at Osiris Trading, and trying to raise above excellence by creating great solutions for the world. 
+Previously, Laurent built software geared towards the efficiency of use of plant machinery in Johannesburg for the BMW Group 
+right after serving as a software developer for a startup named Retro Rabbit.
+`;
+
+const social = `
+Twitter: https://twitter.com/laurent_mb
+Github: https://github.com/geek4ctrl
+`;
+
+const ad = `I'm busy thinking about life`;
+
+module.exports = {
+    bio,
+    social,
+    ad
+}
+
